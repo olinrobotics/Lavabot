18a14f4552105a663fc38bb54b308dbb99aa1efa
Yichen Jiang <pauljyc0929@gmail.com>
2016-12-10 20:44:29 -0500

    Obstacle

--- Obstacles.py
+++ Obstacles.py
@@ -2,16 +2,17 @@ import rospy
 import wiringpi
 from Waypoints import *
 from sensors_msgs.msg import Range
+from Guided import *
+from OdroidToPixhark import *
 
 wiringpi.wiringPiSetupGpio()
 wiringpi.pinMode(37,1)
 min_distance = 100
 
 '''stop the rover by setting and pushing a waypoint at the current location'''
-def stop_motor():
-    stoppoint = gps_callback()
-    make_global_waypoint(stoppoint.latitude,stoppoint.longitude)
-    push_waypoints
+def stop_motor(lat,lon,alt=DEFAULT_ALT):
+    stoppoint = Waypoints.gps_callback()
+    Guided.set_guided_waypoint(stoppoint.lat,stoppoint.lon,stoppoint.alt)
 
 '''sense obstacle with IR sensor'''
 def find_obstacle():
@@ -19,4 +20,4 @@ def find_obstacle():
         pass
     else:
         print 'stop'
-        stop_motor();
+        stop_motor()
