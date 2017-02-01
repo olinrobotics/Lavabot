7c7ee25096ee9e92229cff8992fc5c1b348a8dab
Yichen Jiang <pauljyc0929@gmail.com>
2017-02-01 16:55:07 -0500

    Obstacle detection partially workint

--- Obstacles.py
+++ Obstacles.py
@@ -1,23 +1,23 @@
-import rospy
-import wiringpi
-from Waypoints import *
-from sensors_msgs.msg import Range
-from Guided import *
-from OdroidToPixhark import *
+'''
+Key issues regardign mounting the sensor:
+1. When measuring objects in motion, align the sensor so that the motion is
+    in horizontal direction instead of vertical.
+2. Sources of high ambient light(such as sun light) may affect measurement so
+    mounting should be designed to avoid interference with direct sunlight.
+'''
+'''code for obstacle avoidance with IR sensor'''
+import os
+from JoystickSendWaypoint import waypointCallBack, addWaypoint
 
-wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(37,1)
-min_distance = 100
+def infrared_sensor():
+    cur_reading = 0
+    max_reading = 800
+    while cur_reading<max_reading:
+        data = open("../sys/class/saradc/saradc_ch1")
+        cur_reading = int(data.read())
+        print(cur_reading)
+        data.close
+        print('Find an obstale!')
+        waypointCallBack,add
 
-'''stop the rover by setting and pushing a waypoint at the current location'''
-def stop_motor(lat,lon,alt=DEFAULT_ALT):
-    stoppoint = Waypoints.gps_callback()
-    Guided.set_guided_waypoint(stoppoint.lat,stoppoint.lon,stoppoint.alt)
-
-'''sense obstacle with IR sensor'''
-def find_obstacle():
-    while wiringpi.analogRead(0) > min_distance:
-        pass
-    else:
-        print 'stop'
-        stop_motor()
+infrared_sensor()
